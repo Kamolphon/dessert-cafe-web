c93f8fb4a8e3c10e723d9d5ffe7ed3a0283fa876
Kamolphon <kamolphon.tsw@mail.kmutt.ac.th>
2023-10-23 18:25:25 +0700

    fix font weight

--- src/components/Button/CategoriesBtn.tsx
+++ src/components/Button/CategoriesBtn.tsx
@@ -1,6 +1,6 @@
 function categoriesBtn() {
     return (
-        <div className="md:mx-8 lg:mx-12 w-full flex ml-3 mt-6 md:text-xl text-sm font-light items-center">
+        <div className="md:mx-8 lg:mx-12 w-full flex ml-3 mt-6 md:text-xl text-sm items-center">
             <p className="text-black mr-3 flex">Categories: </p>
             <a className="button-categories md:w-24" href="#Ice Cream">Ice Cream</a>
             <a className="button-categories md:w-24" href="#Cake">Cake</a>
